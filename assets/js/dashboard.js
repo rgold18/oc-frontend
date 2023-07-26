9a4c413bc38ccad93d49d007971639f1c7c79619
rgold18 <34944924+rgold18@users.noreply.github.com>
2023-07-26 14:40:00 -0700

    updated fire ems dash

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,5 +1,5 @@
 /* globals Chart:false */
-
+/*
 (() => {
   'use strict'
 
@@ -46,4 +46,23 @@
       }
     }
   })
-})()
+})()*/ 
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert(`You've triggered the panic button!`, 'danger')
+  })
+}
